d99d6d282566c15077e533d0b83940218838aca0
Nguyen Tran <nguyen.tran2092@gmail.com>
2020-07-16 18:05:42 +0700

    update customer

--- backend/widgets/views/verticalNavWidget.php
+++ backend/widgets/views/verticalNavWidget.php
@@ -30,7 +30,9 @@ $is_dev = Yii::$app->user->can('develop');
                 <span>UI</span>
             </div>
             <ul class="navbar-nav flex-column">
-                <?php if (Yii::$app->user->can(User::DEV) || Yii::$app->user->can('customer')) { ?>
+                <?php if (Yii::$app->user->can(User::DEV) ||
+                    Yii::$app->user->can('customer') ||
+                    Yii::$app->user->can('customerCustomerIndex')) { ?>
                     <li class="nav-item<?php if (Yii::$app->controller->module->id == 'customer') echo ' active'; ?>">
                         <a class="nav-link" href="<?= Url::toRoute(['/customer']); ?>">
                             <i class="ion ion-md-contacts"></i>
@@ -39,44 +41,44 @@ $is_dev = Yii::$app->user->can('develop');
                     </li>
                 <?php } ?>
                 <?php if (Yii::$app->user->can(User::DEV) || Yii::$app->user->can('product')) { ?>
-                <li class="nav-item<?php if (Yii::$app->controller->module->id == 'product') echo ' active'; ?>">
-                    <a class="nav-link" href="<?= Url::toRoute(['/product']); ?>">
-                        <i class="ion ion-md-cube"></i>
-                        <span class="nav-link-text"><?= Yii::t('backend', 'Product'); ?></span>
-                    </a>
-                </li>
+                    <li class="nav-item<?php if (Yii::$app->controller->module->id == 'product') echo ' active'; ?>">
+                        <a class="nav-link" href="<?= Url::toRoute(['/product']); ?>">
+                            <i class="ion ion-md-cube"></i>
+                            <span class="nav-link-text"><?= Yii::t('backend', 'Product'); ?></span>
+                        </a>
+                    </li>
                 <?php } ?>
                 <?php if (Yii::$app->user->can(User::DEV) || Yii::$app->user->can('article')) { ?>
-                <li class="nav-item<?php if (Yii::$app->controller->module->id == 'article') echo ' active'; ?>">
-                    <a class="nav-link" href="<?= Url::toRoute(['/article']); ?>">
-                        <i class="ion ion-ios-book"></i>
-                        <span class="nav-link-text"><?= Yii::t('backend', 'Article'); ?></span>
-                    </a>
-                </li>
+                    <li class="nav-item<?php if (Yii::$app->controller->module->id == 'article') echo ' active'; ?>">
+                        <a class="nav-link" href="<?= Url::toRoute(['/article']); ?>">
+                            <i class="ion ion-ios-book"></i>
+                            <span class="nav-link-text"><?= Yii::t('backend', 'Article'); ?></span>
+                        </a>
+                    </li>
                 <?php } ?>
                 <?php if (Yii::$app->user->can(User::DEV) || Yii::$app->user->can('location')) { ?>
-                <li class="nav-item<?php if (Yii::$app->controller->module->id == 'location') echo ' active'; ?>">
-                    <a class="nav-link" href="<?= Url::toRoute(['/location/location-country']); ?>">
-                        <i class="ion ion-md-map"></i>
-                        <span class="nav-link-text"><?= Yii::t('backend', 'Location'); ?></span>
-                    </a>
-                </li>
+                    <li class="nav-item<?php if (Yii::$app->controller->module->id == 'location') echo ' active'; ?>">
+                        <a class="nav-link" href="<?= Url::toRoute(['/location/location-country']); ?>">
+                            <i class="ion ion-md-map"></i>
+                            <span class="nav-link-text"><?= Yii::t('backend', 'Location'); ?></span>
+                        </a>
+                    </li>
                 <?php } ?>
                 <?php if (Yii::$app->user->can(User::DEV) || Yii::$app->user->can('contact')) { ?>
-                <li class="nav-item<?php if (Yii::$app->controller->module->id == 'contact') echo ' active'; ?>">
-                    <a class="nav-link" href="<?= Url::toRoute(['/contact']); ?>">
-                        <i class="ion ion-md-contact"></i>
-                        <span class="nav-link-text"><?= Yii::t('backend', 'Contact'); ?></span>
-                    </a>
-                </li>
+                    <li class="nav-item<?php if (Yii::$app->controller->module->id == 'contact') echo ' active'; ?>">
+                        <a class="nav-link" href="<?= Url::toRoute(['/contact']); ?>">
+                            <i class="ion ion-md-contact"></i>
+                            <span class="nav-link-text"><?= Yii::t('backend', 'Contact'); ?></span>
+                        </a>
+                    </li>
                 <?php } ?>
                 <?php if (Yii::$app->user->can(User::DEV) || Yii::$app->user->can('marketing')) { ?>
-                <li class="nav-item<?php if (Yii::$app->controller->module->id == 'marketing') echo ' active'; ?>">
-                    <a class="nav-link" href="<?= Url::toRoute(['/marketing']); ?>">
-                        <i class="ion ion-logo-markdown"></i>
-                        <span class="nav-link-text"><?= Yii::t('backend', 'Marketing'); ?></span>
-                    </a>
-                </li>
+                    <li class="nav-item<?php if (Yii::$app->controller->module->id == 'marketing') echo ' active'; ?>">
+                        <a class="nav-link" href="<?= Url::toRoute(['/marketing']); ?>">
+                            <i class="ion ion-logo-markdown"></i>
+                            <span class="nav-link-text"><?= Yii::t('backend', 'Marketing'); ?></span>
+                        </a>
+                    </li>
                 <?php } ?>
                 <?php if ($is_dev || Yii::$app->user->can('authUser')) { ?>
                     <li class="nav-item<?php if (Yii::$app->controller->module->id == 'auth') echo ' active'; ?>">
@@ -87,20 +89,20 @@ $is_dev = Yii::$app->user->can('develop');
                     </li>
                 <?php } ?>
                 <?php if (Yii::$app->user->can(User::DEV) || Yii::$app->user->can('settings')) { ?>
-                <li class="nav-item<?php if (Yii::$app->controller->module->id == 'settings') echo ' active'; ?>">
-                    <a class="nav-link" href="<?= Url::toRoute(['/settings']); ?>">
-                        <i class="ion ion-ios-cog"></i>
-                        <span class="nav-link-text"><?= Yii::t('backend', 'Settings'); ?></span>
-                    </a>
-                </li>
+                    <li class="nav-item<?php if (Yii::$app->controller->module->id == 'settings') echo ' active'; ?>">
+                        <a class="nav-link" href="<?= Url::toRoute(['/settings']); ?>">
+                            <i class="ion ion-ios-cog"></i>
+                            <span class="nav-link-text"><?= Yii::t('backend', 'Settings'); ?></span>
+                        </a>
+                    </li>
                 <?php } ?>
                 <?php if (Yii::$app->user->can(User::DEV) || Yii::$app->user->can('website')) { ?>
-                <li class="nav-item<?php if (Yii::$app->controller->module->id == 'website') echo ' active'; ?>">
-                    <a class="nav-link" href="<?= Url::toRoute(['/website']); ?>">
-                        <i class="ion zmdi-directions-walk"></i>
-                        <span class="nav-link-text"><?= Yii::t('backend', 'Website'); ?></span>
-                    </a>
-                </li>
+                    <li class="nav-item<?php if (Yii::$app->controller->module->id == 'website') echo ' active'; ?>">
+                        <a class="nav-link" href="<?= Url::toRoute(['/website']); ?>">
+                            <i class="ion zmdi-directions-walk"></i>
+                            <span class="nav-link-text"><?= Yii::t('backend', 'Website'); ?></span>
+                        </a>
+                    </li>
                 <?php } ?>
             </ul>
             <hr class="nav-separator">
