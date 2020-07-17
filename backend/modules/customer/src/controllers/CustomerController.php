73eea2e59bd3a0798659c129a5b8c83b69bfc626
Nguyen Tran <nguyen.tran2092@gmail.com>
2020-07-17 11:08:44 +0700

    customer

--- backend/modules/customer/src/controllers/CustomerController.php
+++ backend/modules/customer/src/controllers/CustomerController.php
@@ -27,6 +27,8 @@ class CustomerController extends MyController
     {
         $user_sales_online = KeyValueTable::getValueByKey('ROLE_SALES_ONLINE');
         $user_direct = KeyValueTable::getValueByKey('ROLE_DIRECT');
+        Yii::$app->view->params['user_sales_online'] = $user_sales_online;
+        Yii::$app->view->params['user_direct'] = $user_direct;
         if (Yii::$app->user->can(User::DEV)) $this->s = Customer::SCENARIO_ADMIN;
         else if ($user_sales_online != null && Yii::$app->user->can($user_sales_online)) $this->s = Customer::SCENARIO_ONLINE;
         else if ($user_direct != null && Yii::$app->user->can($user_direct)) $this->s = Customer::SCENARIO_CLINIC;
