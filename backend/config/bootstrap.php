c5d5aef4f628870df50a3b8901061869de9cf9c0
Mong Dao <mongdaovan86@yahoo.com>
2020-07-27 18:56:18 +0700

    start package giaohang

--- backend/config/bootstrap.php
+++ backend/config/bootstrap.php
@@ -27,6 +27,7 @@ Yii::setAlias('modava/voip24h', dirname(dirname(__DIR__)) . '/backend/widgets/vo
 Yii::setAlias('modava/tiny', dirname(dirname(__DIR__)) . '/backend/widgets/tiny/src');
 Yii::setAlias('modava/select2', dirname(dirname(__DIR__)) . '/common/widgets/select2/src');
 Yii::setAlias('modava/imagick', dirname(dirname(__DIR__)) . '/package/imagick/src');
+Yii::setAlias('modava/giaohang', dirname(dirname(__DIR__)) . '/package/giaohang/src');
 
 
 Yii::setAlias('modava/generators', dirname(dirname(__DIR__)) . '/backend/modava/src/generators');
