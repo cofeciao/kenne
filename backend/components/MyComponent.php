0ea820a991d605e70699d62590a44f80a1e41f9e
Nguyen Tran <nguyen.tran2092@gmail.com>
2020-07-27 14:33:57 +0700

    component cookie

--- backend/components/MyComponent.php
+++ backend/components/MyComponent.php
@@ -8,35 +8,10 @@
 
 namespace backend\components;
 
+use common\components\Component;
 use Yii;
 
-class MyComponent
+class MyComponent extends Component
 {
-    public static function setCookies($name, $value, $time = null)
-    {
-        $cookies = Yii::$app->response->cookies;
-        if ($time == null) {
-            $time = time() + 86400 * 365;
-        }
-        $cookies->add(new \yii\web\Cookie([
-            'name' => $name,
-            'value' => $value,
-            'expire' => $time,
-        ]));
-    }
 
-    public static function getCookies($name)
-    {
-        $cookies = Yii::$app->request->cookies;
-        if (isset($cookies[$name])) {
-            return $cookies[$name]->value;
-        }
-        return false;
-    }
-
-    public static function hasCookies($name)
-    {
-        $cookies = Yii::$app->request->cookies;
-        return $cookies->has($name);
-    }
 }
