72d79c3b8f1fa6000d956059e3b6285276327fa0
thien nhan <1651120111@sv.ut.edu.vn>
2020-07-28 15:23:02 +0700

    merge master

---
+++ package/giaohang/src/GiaoHangTietKiem.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace modava\giaohang;
+
+
+class GiaoHangTietKiem
+{
+    /*
+     * Host giao hang tiet kiem
+     */
+    private $host = 'services.giaohangtietkiem.vn';
+
+    /*
+     * Token
+     */
+    public $token;
+
+    /*
+     * address
+     */
+    public $order;
+
+    public function __construct()
+    {
+
+    }
+
+
+
+}
