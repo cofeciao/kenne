33cfc6139bc7fbf86cb853a9c8654bbc768071b0
Mong Dao <mongdaovan86@yahoo.com>
2020-07-30 15:24:52 +0700

    edit package datetime

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -45,6 +45,7 @@ Yii::setAlias('modava/tiny', dirname(dirname(__DIR__)) . '/backend/widgets/tiny/
 Yii::setAlias('modava/select2', dirname(dirname(__DIR__)) . '/common/widgets/select2/src');
 Yii::setAlias('modava/imagick', dirname(dirname(__DIR__)) . '/package/imagick/src');
 Yii::setAlias('modava/giaohang', dirname(dirname(__DIR__)) . '/package/giaohang/src');
+Yii::setAlias('modava/datetime', dirname(dirname(__DIR__)) . '/backend/widgets/datetime/src');
 
 
 Yii::setAlias('modava/generators', dirname(dirname(__DIR__)) . '/backend/modava/src/generators');
