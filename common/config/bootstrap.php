4948980ef36f095a29087bdc15d0a2e9da8c9f2a
Mong Dao <mongdaovan86@yahoo.com>
2020-08-07 16:07:58 +0700

    update module auth

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -20,7 +20,7 @@ Yii::setAlias('@backendUrl', $domain . '/backend/');
 //Config package
 Yii::setAlias('@moduleBackend', realpath(__DIR__ . '/../backend/modules/'));
 Yii::setAlias('@api', dirname(dirname(__DIR__)) . '/../backend/modules/api');
-//Yii::setAlias('modava/auth', dirname(dirname(__DIR__)) . '/backend/modules/auth/src');
+Yii::setAlias('modava/auth', dirname(dirname(__DIR__)) . '/backend/modules/auth/src');
 Yii::setAlias('modava/contact', dirname(dirname(__DIR__)) . '/backend/modules/contact/src');
 Yii::setAlias('modava/article', dirname(dirname(__DIR__)) . '/backend/modules/article/src');
 Yii::setAlias('modava/product', dirname(dirname(__DIR__)) . '/backend/modules/product/src');
