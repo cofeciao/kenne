e9d9407a90296df4e0df125c9f56da5eafd80fc5
thien nhan <1651120111@sv.ut.edu.vn>
2020-08-08 17:52:45 +0700

    revert accountController

---
+++ frontend/controllers/AccountController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace frontend\controllers;
+
+use frontend\components\MyController;
+use frontend\models\Logo;
+use Yii;
+
+class AccountController extends MyController
+{
+    public function actionIndex(){
+        return $this->render('index',[
+        ]);
+    }
+}
