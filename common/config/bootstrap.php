644aba993fce520d996b097db3830160cb89e1be
Mong Dao <mongdaovan86@yahoo.com>
2020-08-13 19:16:23 +0700

    update add input

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -41,6 +41,7 @@ Yii::setAlias('modava/video', dirname(dirname(__DIR__)) . '/backend/modules/vide
 Yii::setAlias('modava/slide', dirname(dirname(__DIR__)) . '/backend/modules/slide/src');
 Yii::setAlias('modava/faq', dirname(dirname(__DIR__)) . '/backend/modules/faq/src');
 Yii::setAlias('modava/voip24h', dirname(dirname(__DIR__)) . '/backend/widgets/voip24h/src');
+Yii::setAlias('modava/input', dirname(dirname(__DIR__)) . '/backend/widgets/input/src');
 //Yii::setAlias('modava/location', dirname(dirname(__DIR__)) . '/backend/modules/location/src');
 
 //Widget
