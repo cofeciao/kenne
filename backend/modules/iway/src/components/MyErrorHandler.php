13ea8c56be155ade827da90f59b7f122d4d28e68
Mong Dao <mongdaovan86@yahoo.com>
2020-08-14 18:22:26 +0700

    delete iway

--- backend/modules/iway/src/components/MyErrorHandler.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace modava\iway\components;
-
-class MyErrorHandler extends \yii\web\ErrorHandler
-{
-    public $errorView = '@modava/iway/views/error/error.php';
-
-}
--- backend/modules/iway/src/views/layouts/iway.php
+++
@@ -1,7 +0,0 @@
-<?php
-\modava\iway\assets\IwayAsset::register($this);
-\modava\iway\assets\IwayCustomAsset::register($this);
-?>
-<?php $this->beginContent('@backend/views/layouts/main.php'); ?>
-<?php echo $content ?>
-<?php $this->endContent(); ?>
