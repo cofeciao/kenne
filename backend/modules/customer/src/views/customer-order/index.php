d99fbcd0d7408e8352688a9ab67cafd23d9a5460
Tam Nguyen <tam.nguyen@365dep.vn>
2020-08-25 20:07:22 +0700

    customer

--- backend/modules/customer/src/views/customer-order/index.php
+++ backend/modules/customer/src/views/customer-order/index.php
@@ -3,7 +3,7 @@
 use modava\customer\CustomerModule;
 use modava\customer\widgets\NavbarWidgets;
 use yii\helpers\Html;
-use yii\grid\GridView;
+use common\grid\MyGridView;
 use backend\widgets\ToastrWidget;
 use yii\widgets\Pjax;
 use modava\customer\models\table\CustomerOrderTable;
@@ -25,7 +25,7 @@ $this->params['breadcrumbs'][] = $this->title;
             <h4 class="hk-pg-title"><span class="pg-title-icon"><span
                             class="ion ion-md-apps"></span></span><?= Html::encode($this->title) ?>
             </h4>
-            <a class="btn btn-outline-light"
+            <a class="btn btn-outline-light btn-sm"
                href="<?= \yii\helpers\Url::to(['create', 'customer_id' => Yii::$app->request->get('customer_id')]); ?>"
                title="<?= CustomerModule::t('customer', 'Create'); ?>">
                 <i class="fa fa-plus"></i> <?= CustomerModule::t('customer', 'Create'); ?></a>
@@ -36,33 +36,39 @@ $this->params['breadcrumbs'][] = $this->title;
             <div class="col-xl-12">
                 <section class="hk-sec-wrapper">
 
-                    <?php Pjax::begin(); ?>
+                    <?php Pjax::begin(['id' => 'customer-pjax', 'timeout' => false, 'enablePushState' => true, 'clientOptions' => ['method' => 'GET']]); ?>
                     <div class="row">
                         <div class="col-sm">
                             <div class="table-wrap">
                                 <div class="dataTables_wrapper dt-bootstrap4">
-                                    <?= GridView::widget([
+                                    <?= MyGridView::widget([
                                         'dataProvider' => $dataProvider,
                                         'layout' => '
-                                        {errors}
-                                        <div class="row">
-                                            <div class="col-sm-12">
+                                            {errors} 
+                                            <div class="pane-single-table">
                                                 {items}
                                             </div>
-                                        </div>
-                                        <div class="row">
-                                            <div class="col-sm-12 col-md-5">
-                                                <div class="dataTables_info" role="status" aria-live="polite">
-                                                    {pager}
-                                                </div>
+                                            <div class="pager-wrap clearfix">
+                                                {summary}' .
+                                            Yii::$app->controller->renderPartial('@backend/views/layouts/my-gridview/_pageTo', [
+                                                'totalPage' => $totalPage,
+                                                'currentPage' => Yii::$app->request->get($dataProvider->getPagination()->pageParam)
+                                            ]) .
+                                            Yii::$app->controller->renderPartial('@backend/views/layouts/my-gridview/_pageSize') .
+                                            '{pager}
                                             </div>
-                                            <div class="col-sm-12 col-md-7">
-                                                <div class="dataTables_paginate paging_simple_numbers">
-                                                    {summary}
-                                                </div>
-                                            </div>
-                                        </div>
-                                    ',
+                                        ',
+                                        'tableOptions' => [
+                                            'id' => 'dataTable',
+                                            'class' => 'dt-grid dt-widget pane-hScroll',
+                                        ],
+                                        'myOptions' => [
+                                            'class' => 'dt-grid-content my-content pane-vScroll',
+                                            'data-minus' => '{"0":120,"1":".hk-navbar","2":".nav-tabs","3":".hk-pg-header","4":".hk-footer-wrap"}'
+                                        ],
+                                        'summaryOptions' => [
+                                            'class' => 'summary pull-right',
+                                        ],
                                         'pager' => [
                                             'firstPageLabel' => CustomerModule::t('customer', 'First'),
                                             'lastPageLabel' => CustomerModule::t('customer', 'Last'),
@@ -82,10 +88,10 @@ $this->params['breadcrumbs'][] = $this->title;
                                             'pageCssClass' => 'page-item',
 
                                             // Customzing CSS class for navigating link
-                                            'prevPageCssClass' => 'paginate_button page-item',
-                                            'nextPageCssClass' => 'paginate_button page-item',
-                                            'firstPageCssClass' => 'paginate_button page-item',
-                                            'lastPageCssClass' => 'paginate_button page-item',
+                                            'prevPageCssClass' => 'paginate_button page-item prev',
+                                            'nextPageCssClass' => 'paginate_button page-item next',
+                                            'firstPageCssClass' => 'paginate_button page-item first',
+                                            'lastPageCssClass' => 'paginate_button page-item last',
                                         ],
                                         'columns' => [
                                             [
@@ -220,7 +226,13 @@ $this->params['breadcrumbs'][] = $this->title;
         </div>
     </div>
 <?php
+$urlChangePageSize = \yii\helpers\Url::toRoute(['perpage']);
 $script = <<< JS
+var customPjax = new myGridView();
+customPjax.init({
+    pjaxId: '#customer-pjax',
+    urlChangePageSize: '$urlChangePageSize',
+});
 $('body').on('click', '.success-delete', function(e){
     e.preventDefault();
     var url = $(this).attr('href') || null;
