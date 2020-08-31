120b49892ce7c9c34e84955f65911a720b7e8505
huynhlehoangduc <huynhlehoangduc@gmail.com>
2020-08-31 10:35:08 +0700

    Add chart dashboard

--- backend/modules/affiliate/src/config/params.php
+++ backend/modules/affiliate/src/config/params.php
@@ -1,6 +1,7 @@
 <?php
 use modava\affiliate\AffiliateModule;
 use modava\affiliate\models\Coupon;
+use modava\affiliate\models\Customer;
 
 return [
     'affiliateName' => 'Affiliate',
@@ -19,8 +20,12 @@ return [
         '2' => AffiliateModule::t('affiliate', 'Other'),
     ],
     'customer_status' => [
-        '0' => AffiliateModule::t('affiliate', 'Đang làm dịch vụ'),
-        '1' => AffiliateModule::t('affiliate', 'Đã hoàn thành dịch vụ'),
+        Customer::STATUS_DANG_LAM_DICH_VU => AffiliateModule::t('affiliate', 'Đang làm dịch vụ'),
+        Customer::STATUS_HOAN_THANH_DICH_VU => AffiliateModule::t('affiliate', 'Đã hoàn thành dịch vụ'),
+    ],
+    'customer_status_color' => [
+        Customer::STATUS_DANG_LAM_DICH_VU => '#69c982',
+        Customer::STATUS_HOAN_THANH_DICH_VU => '#22af47',
     ],
     'promotion_type' => [
         Coupon::DISCOUNT_PERCENT => AffiliateModule::t('affiliate', 'Discount Percent In Order'),
@@ -57,6 +62,11 @@ return [
         '0' => AffiliateModule::t('affiliate', 'Unsatisfied'),
         '1' => AffiliateModule::t('affiliate', 'Satisfied'),
         '2' => AffiliateModule::t('affiliate', 'Bình thường'),
+    ],
+    'feedback_type_color' => [
+        '0' => '#ab26aa',
+        '1' => '#22af47',
+        '2' => '#52b5f3',
     ],
     'receipt_status' => [
         '0' => AffiliateModule::t('affiliate', 'Thanh toán'),
--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -6,6 +6,11 @@
  * Time: 4:24 PM
  */
 
+use yii\helpers\Url;
+
+use modava\chart\PieChart;
+use modava\chart\BarChart;
+use modava\affiliate\AffiliateModule;
 
 $this->title = 'Dashboard';
 $this->params['breadcrumbs'][] = $this->title;
@@ -35,8 +40,39 @@ $this->params['breadcrumbs'][] = $this->title;
         <div class="tab-pane active" id="tabs-1" role="tabpanel">
             <!-- Row -->
             <div class="row">
-                <div class="col-xl-12">
-                    Content Here
+                <div class="col-xl-6">
+                    <?= BarChart::widget([
+                        'title' => AffiliateModule::t('affliate', 'Feedback theo giai đoạn'),
+                        'url_get_data' => Url::toRoute(["/affiliate/affiliate/get-total-feedback-by-type-and-time?type=week"]),
+                    ]) ?>
+                </div>
+                <div class="col-xl-6">
+                    <?= PieChart::widget([
+                        'title' => AffiliateModule::t('affliate', 'Tổng Feedback'),
+                        'url_get_data' => Url::toRoute(["/affiliate/affiliate/get-total-feedback-by-type?type=''"]),
+                        'unit_title' => 'FB'
+                    ]) ?>
+                </div>
+                <div class="col-xl-6">
+                    <?= PieChart::widget([
+                        'title' => AffiliateModule::t('affliate', 'Khách hàng đã chuyển đổi trong tháng'),
+                        'url_get_data' => Url::toRoute(["/affiliate/affiliate/get-total-convert?type=month"]),
+                        'unit_title' => 'KH'
+                    ]) ?>
+                </div>
+                <div class="col-xl-6">
+                    <?= PieChart::widget([
+                        'title' => AffiliateModule::t('affliate', 'Khách hàng đã chuyển đổi trong năm'),
+                        'url_get_data' => Url::toRoute(["/affiliate/affiliate/get-total-convert?type=year"]),
+                        'unit_title' => 'KH'
+                    ]) ?>
+                </div>
+                <div class="col-xl-6">
+                    <?= PieChart::widget([
+                        'title' => AffiliateModule::t('affliate', 'Khách hàng đã chuyển đổi trong tuần'),
+                        'url_get_data' => Url::toRoute(["/affiliate/affiliate/get-total-convert?type=week"]),
+                        'unit_title' => 'KH'
+                    ]) ?>
                 </div>
             </div>
             <!-- /Row -->
