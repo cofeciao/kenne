c6efe68ca99583b5eefa54c7b1539fbe8da4e7b4
huynhlehoangduc <huynhlehoangduc@gmail.com>
2020-09-01 16:19:58 +0700

    Replace translate function, Apply widget minilist

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -11,7 +11,6 @@ use yii\helpers\Url;
 use modava\chart\PieChart;
 use modava\chart\MiniList;
 use modava\chart\BarChart;
-use modava\affiliate\AffiliateModule;
 
 $this->title = 'Dashboard';
 $this->params['breadcrumbs'][] = $this->title;
@@ -43,49 +42,49 @@ $this->params['breadcrumbs'][] = $this->title;
             <div class="row">
                 <div class="col-xl-6">
                     <?= MiniList::widget([
-                        'title' => AffiliateModule::t('affiliate', 'Note cần gọi trong ngày'),
+                        'title' => Yii::t('backend', 'Note cần gọi trong ngày'),
                         'columns' => [
                             'title',
                             'Khách hàng',
                             'SĐT',
+                            'Đã gọi lại',
                             'Thời gian gọi',
                             'Thời gian gọi lại',
                             'Note',
-                            'Đã gọi lại',
                         ],
-                        'url_get_data' => Url::toRoute(["/affiliate/note/get-coming-note"])
+                        'url_get_data' => Url::toRoute(["/affiliate/note/get-coming-note"]),
                     ]) ?>
                 </div>
                 <div class="col-xl-6">
                     <?= BarChart::widget([
-                        'title' => AffiliateModule::t('affliate', 'Feedback theo giai đoạn'),
+                        'title' => Yii::t('affliate', 'Feedback theo giai đoạn'),
                         'url_get_data' => Url::toRoute(["/affiliate/affiliate/get-total-feedback-by-type-and-time?type=week"]),
                     ]) ?>
                 </div>
                 <div class="col-xl-6">
                     <?= PieChart::widget([
-                        'title' => AffiliateModule::t('affliate', 'Tổng Feedback'),
+                        'title' => Yii::t('affliate', 'Tổng Feedback'),
                         'url_get_data' => Url::toRoute(["/affiliate/affiliate/get-total-feedback-by-type?type=''"]),
                         'unit_title' => 'FB'
                     ]) ?>
                 </div>
                 <div class="col-xl-6">
                     <?= PieChart::widget([
-                        'title' => AffiliateModule::t('affliate', 'Khách hàng đã chuyển đổi trong tháng'),
+                        'title' => Yii::t('affliate', 'Khách hàng đã chuyển đổi trong tháng'),
                         'url_get_data' => Url::toRoute(["/affiliate/affiliate/get-total-convert?type=month"]),
                         'unit_title' => 'KH'
                     ]) ?>
                 </div>
                 <div class="col-xl-6">
                     <?= PieChart::widget([
-                        'title' => AffiliateModule::t('affliate', 'Khách hàng đã chuyển đổi trong năm'),
+                        'title' => Yii::t('affliate', 'Khách hàng đã chuyển đổi trong năm'),
                         'url_get_data' => Url::toRoute(["/affiliate/affiliate/get-total-convert?type=year"]),
                         'unit_title' => 'KH'
                     ]) ?>
                 </div>
                 <div class="col-xl-6">
                     <?= PieChart::widget([
-                        'title' => AffiliateModule::t('affliate', 'Khách hàng đã chuyển đổi trong tuần'),
+                        'title' => Yii::t('affliate', 'Khách hàng đã chuyển đổi trong tuần'),
                         'url_get_data' => Url::toRoute(["/affiliate/affiliate/get-total-convert?type=week"]),
                         'unit_title' => 'KH'
                     ]) ?>
