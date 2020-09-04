3015cb5b963f134528d5947e1dcd505df59bffd2
huynhlehoangduc <huynhlehoangduc@gmail.com>
2020-09-04 10:03:54 +0700

    Modify foreignkey receipt order

--- backend/modules/affiliate/src/migration/m200903_030004_modiy_table_affiliate_order.php
+++ backend/modules/affiliate/src/migration/m200903_030004_modiy_table_affiliate_order.php
@@ -13,6 +13,8 @@ class m200903_030004_modiy_table_affiliate_order extends Migration
     public function safeUp()
     {
         $this->addColumn('affiliate_order', 'other_discount', $this->decimal(11)->defaultValue(0)->comment('Các khuyến mãi khác'));
+        $this->dropForeignKey('fk_af_receipt_order_id_af_order_id', 'affiliate_receipt');
+        $this->addForeignKey('fk_af_receipt_order_id_af_order_id', 'affiliate_receipt', 'order_id', 'affiliate_order', 'id','CASCADE', 'CASCADE');
     }
 
     /**
