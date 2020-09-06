3c5b796961825e07bb04abe1dc98e2e23b318cf6
huynhlehoangduc <huynhlehoangduc@gmail.com>
2020-09-07 01:00:39 +0700

    Add reapply migration for iway

--- backend/modules/iway/src/migration/m200824_155442_create_table_iway_customer.php
+++ backend/modules/iway/src/migration/m200824_155442_create_table_iway_customer.php
@@ -23,45 +23,40 @@ class m200824_155442_create_table_iway_customer extends Migration
             $this->createTable('iway_customer', [
                 'id' => $this->primaryKey(),
                 'code' => $this->string(255)->null(),
-                'name' => $this->string(255)->notNull(),
-                'birthday' => $this->date()->null(),
-                'sex' => $this->string()->null(),
+                'fullname' => $this->string(255)->notNull(),
+                'avatar' => $this->string(255)->null(),
                 'phone' => $this->string(30)->notNull(),
+                'sex' => $this->string()->null(),
+                'birthday' => $this->date()->null(),
                 'address' => $this->string(255)->null(),
-                'country_id' => $this->integer(11)->null()->comment('Quốc gia'),
                 'province_id' => $this->integer(11)->null()->comment('Tỉnh/Thành phố'),
                 'district_id' => $this->integer(11)->null()->comment('Quận/Huyện'),
                 'ward_id' => $this->integer(11)->null()->comment('Phường/Xã'),
-                'avatar' => $this->string(255)->null(),
-                'fanpage_id' => $this->integer(11)->null(),
-                'type' => $this->string()->notNull()->comment('Chưa xác định - Khách online - Khách vãng lai ... '),
-                'online_sales_id' => $this->integer(11)->notNull()->comment('Sales Online'),
-                'sale_online_note' => $this->text()->null()->comment('Ghi chú của Sales Online'),
-                'direct_sale_id' => $this->integer(11)->null()->comment('Direct Sale phụ trách'),
-                'direct_sale_note' => $this->text()->null()->comment('Ghi chú của Direct Sale'),
-                'co_so' => $this->integer(11)->null(),
-                'status' => $this->string()->null()->comment('Tình trạng khách hàng'),
+                'online_source' => $this->string(50)->null()->comment('Nguồn online'),
+                'fb_fanpage' => $this->string(255)->null()->comment('Nếu khách đến từ nguồn FB, thì đến từ Fanpage nào'),
+                'fb_customer' => $this->string(255)->null()->comment('Nếu khách đến từ nguồn FB, thì fb của khách là gì'),
+                'online_sales_id' => $this->integer(11)->notNull()->comment('Sales Online Tư vấn'),
+                'online_sales_note' => $this->text()->null()->comment('Ghi chú của Sales Online'),
+                'direct_sales_id' => $this->integer(11)->null()->comment('Direct Sale phụ trách'),
+                'direct_sales_note' => $this->text()->null()->comment('Ghi chú của Direct Sale'),
+                'status_customer' => $this->string(50)->null()->comment('Tình trạng khách hàng: Đặt hẹn, Chờ chăm sóc lại, Fail'),
+                'co_so_id' => $this->integer(11)->notNull()->comment('Cơ sở'),
+                'reason_fail' => $this->string(255)->null()->comment('lý do fail'),
+                'who_created' => $this->string(50)->null()->comment('Người tạo: Lễ tân, Online, Direct, Khác'),
 
-                /*'status_call' => $this->string()->null()->comment('KBM - Fail - Đặt hẹn'),
-                'status_fail' => $this->string()->null()->comment('Tiềm năng - Ở xa - Có con nhỏ ...'),
-                'status_dat_hen' => $this->string()->null()->comment('Đặt hẹn đến - Đặt hẹn không đến'),
-                'status_dong_y' => $this->string()->null()->comment('Đồng ý - Không đồng ý - Làm dịch vụ khác'),
-                'remind_call_time' => $this->integer()->null()->comment('Khi nào nên gọi lại.'),
-                'time_lich_hen' => $this->integer(11)->null()->comment('Thời gian lịch hẹn'),
-                'time_come' => $this->integer(11)->null()->comment('Thời gian khách đến'),*/
-
+                'description' => $this->text()->null(),
                 'created_at' => $this->integer(11)->null(),
                 'created_by' => $this->integer(11)->null(),
                 'updated_at' => $this->integer(11)->null(),
                 'updated_by' => $this->integer(11)->null(),
             ], $tableOptions);
-            $this->addForeignKey('fk_iway_fanpage_id_customer_fanpage', 'iway_customer', 'fanpage_id', 'customer_fanpage', 'id', 'RESTRICT', 'CASCADE');
+
             $this->addForeignKey('fk_iway_customer_online_sales_id_user', 'iway_customer', 'online_sales_id', 'user', 'id', 'RESTRICT', 'CASCADE');
-            $this->addForeignKey('fk_iway_customer_direct_sale_user', 'iway_customer', 'direct_sale_id', 'user', 'id', 'RESTRICT', 'CASCADE');
-            $this->addForeignKey('fk_iway_co_so_customer_co_so', 'iway_customer', 'co_so', 'customer_co_so', 'id', 'RESTRICT', 'CASCADE');
+            $this->addForeignKey('fk_iway_customer_directs_sale_user', 'iway_customer', 'direct_sales_id', 'user', 'id', 'RESTRICT', 'CASCADE');
+
             $this->addForeignKey('fk_iway_customer_created_by_user', 'iway_customer', 'created_by', 'user', 'id', 'RESTRICT', 'CASCADE');
             $this->addForeignKey('fk_iway_customer_updated_by_user', 'iway_customer', 'updated_by', 'user', 'id', 'RESTRICT', 'CASCADE');
-            $this->addForeignKey('fk_iway_customer__country_id-location_country__id', 'iway_customer', 'country_id', 'location_country', 'id');
+
             $this->addForeignKey('fk_iway_customer__province_id-location_province__id', 'iway_customer', 'province_id', 'location_province', 'id');
             $this->addForeignKey('fk_iway_customer__district_id-location_district__id', 'iway_customer', 'district_id', 'location_district', 'id');
             $this->addForeignKey('fk_iway_customer__ward_id-location_ward__id', 'iway_customer', 'ward_id', 'location_ward', 'id');
--- backend/modules/iway/src/migration/m200906_170704_create_table_iway_co_so.php
+++ backend/modules/iway/src/migration/m200906_170704_create_table_iway_co_so.php
@@ -3,9 +3,9 @@
 use yii\db\Migration;
 
 /**
- * Class m200824_155134_create_table_iway_co_so
+ * Class m200906_170704_create_table_iway_co_so
  */
-class m200824_155134_create_table_iway_co_so extends Migration
+class m200906_170704_create_table_iway_co_so extends Migration
 {
     /**
      * {@inheritdoc}
@@ -13,8 +13,8 @@ class m200824_155134_create_table_iway_co_so extends Migration
     public function safeUp()
     {
         /* check table exists */
-        $check_table = Yii::$app->db->getTableSchema('iway_co_so');
-        if ($check_table === null) {
+        $check_table_customer = Yii::$app->db->getTableSchema('iway_co_so');
+        if ($check_table_customer === null) {
             $tableOptions = null;
             if ($this->db->driverName === 'mysql') {
                 // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
@@ -22,21 +22,19 @@ class m200824_155134_create_table_iway_co_so extends Migration
             }
             $this->createTable('iway_co_so', [
                 'id' => $this->primaryKey(),
-                'name' => $this->string(255)->null(),
-                'address' => $this->string(255)->null(),
-                'phone' => $this->string(255)->null(),
-                'email' => $this->string(255)->null(),
-                'description' => $this->string(255)->null(),
-                'status' => $this->tinyInteger(1)->null()->defaultValue(1)->comment('0:disabled, 1:activated'),
+                'title' => $this->string()->notNull(),
+                'status' => $this->smallInteger(1)->comment('Tình trạng: Hoạt động, Không hoạt động'),
                 'created_at' => $this->integer(11)->null(),
+                'created_by' => $this->integer(11)->null(),
                 'updated_at' => $this->integer(11)->null(),
-                'created_by' => $this->integer(11)->null()->defaultValue(1),
-                'updated_by' => $this->integer(11)->null()->defaultValue(1),
+                'updated_by' => $this->integer(11)->null(),
             ], $tableOptions);
-            $this->addColumn('iway_co_so', 'language', "ENUM('vi', 'en', 'jp') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'vi' COMMENT 'Language' AFTER `status`");
-            $this->createIndex('index-language', 'iway_co_so', 'language');
+
             $this->addForeignKey('fk_iway_co_so_created_by_user', 'iway_co_so', 'created_by', 'user', 'id', 'RESTRICT', 'CASCADE');
             $this->addForeignKey('fk_iway_co_so_updated_by_user', 'iway_co_so', 'updated_by', 'user', 'id', 'RESTRICT', 'CASCADE');
+
+            $this->addForeignKey('fk_iway_customer_co_so_id_iway_co_so_id', 'iway_customer', 'co_so_id', 'iway_co_so', 'id');
+            $this->addForeignKey('fk_iway_appointment_schedule_co_so_id_iway_co_so_id', 'iway_appointment_schedule', 'co_so_id', 'iway_co_so', 'id');
         }
     }
 
@@ -45,7 +43,7 @@ class m200824_155134_create_table_iway_co_so extends Migration
      */
     public function safeDown()
     {
-        echo "m200824_155134_create_table_iway_co_so cannot be reverted.\n";
+        echo "m200906_170704_create_table_iway_co_so cannot be reverted.\n";
 
         return false;
     }
