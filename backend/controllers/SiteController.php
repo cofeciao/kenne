6bbf0707cd99dbf6a7b8ab17d801fa54fb056506
huynhlehoangduc <huynhlehoangduc@gmail.com>
2020-09-08 14:50:50 +0700

    Update Base LineChart (update missing file)

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -40,36 +40,41 @@ class SiteController extends MyController
                 'y' => '500', 'a' => 10, 'b' => 40, 'c' => 100],
 
         ];
-//        return [
-//            [
-//                'period' => "2010",
-//                'iphone' => '50',
-//            ],
-//            [
-//                'period' => "2011",
-//                'iphone' => 130,
-//            ],
-//            [
-//                'period' => "2012",
-//                'iphone' => 80,
-//            ],
-//            [
-//                'period' => "2013",
-//                'iphone' => 70,
-//            ],
-//            [
-//                'period' => "2014",
-//                'iphone' => 180,
-//            ],
-//            [
-//                'period' => "2015",
-//                'iphone' => 105,
-//            ],
-//            [
-//                'period' => "2016",
-//                'iphone' => 250,
-//            ],
-//        ];
+    }
+    public function actionGetDataLineCharts()
+    {
+        Yii::$app->response->format = Response::FORMAT_JSON;
+
+        return [
+            [
+                'period' => "2010",
+                'iphone' => '50',
+            ],
+            [
+                'period' => "2011",
+                'iphone' => 130,
+            ],
+            [
+                'period' => "2012",
+                'iphone' => 80,
+            ],
+            [
+                'period' => "2013",
+                'iphone' => 70,
+            ],
+            [
+                'period' => "2014",
+                'iphone' => 180,
+            ],
+            [
+                'period' => "2015",
+                'iphone' => 105,
+            ],
+            [
+                'period' => "2016",
+                'iphone' => 250,
+            ],
+        ];
     }
 
     public function actionError()
