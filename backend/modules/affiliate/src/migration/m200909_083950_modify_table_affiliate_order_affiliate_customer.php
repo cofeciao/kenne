0ccc48a45b020be264801244b32929be69a804a2
huynhlehoangduc <huynhlehoangduc@gmail.com>
2020-09-09 17:52:09 +0700

    Update migration

--- backend/modules/affiliate/src/migration/m200909_083950_modify_table_affiliate_order_affiliate_customer.php
+++ backend/modules/affiliate/src/migration/m200909_083950_modify_table_affiliate_order_affiliate_customer.php
@@ -19,6 +19,8 @@ class m200909_083950_modify_table_affiliate_order_affiliate_customer extends Mig
         $this->addColumn('affiliate_customer', 'bank_name', $this->string()->null()->comment('Tên ngân hàng'));
         $this->addColumn('affiliate_customer', 'bank_branch', $this->string()->null()->comment('Chi nhánh ngân hàng'));
         $this->addColumn('affiliate_customer', 'bank_customer_id', $this->string(35)->null()->comment('Số tài khoản'));
+        $this->addColumn('affiliate_customer', 'user_portal_id', $this->integer(11)->null()->comment('Id của tài khoản đăng nhập'));
+        $this->addForeignKey('fk_af_customer_user_portal_id_user_id', 'affiliate_customer', 'user_portal_id', 'user', 'id');
     }
 
     /**
