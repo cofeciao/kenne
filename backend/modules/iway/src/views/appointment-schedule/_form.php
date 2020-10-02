1ef67752f2eaadbbd588b5d9987d45f88aa6e690
huynhlehoangduc <huynhlehoangduc@gmail.com>
2020-10-02 15:34:31 +0700

    Update Lich Hen

--- backend/modules/iway/src/views/appointment-schedule/_form.php
+++ backend/modules/iway/src/views/appointment-schedule/_form.php
@@ -172,15 +172,14 @@ $user = User::findOne(Yii::$app->user->id);
                 </div>
             <?php endif; ?>
         </div>
+
+        <div class="form-group">
+            <?= Html::submitButton(Yii::t('backend', 'Save'), ['class' => 'btn btn-success']) ?>
+        </div>
     </section>
 </div>
 
-<div class="form-group">
-    <?= Html::submitButton(Yii::t('backend', 'Save'), ['class' => 'btn btn-success']) ?>
-</div>
-
 <?php ActiveForm::end(); ?>
-</div>
 
 <?= JsCreateModalWidget::widget(['formClassName' => 'lich_hen_form', 'modelName' => 'AppointmentSchedule']) ?>
 
