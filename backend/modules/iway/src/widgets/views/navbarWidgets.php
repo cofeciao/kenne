7b4bb722b916cecc2b9f3608603ec4bc22fa5b24
huynhlehoangduc <huynhlehoangduc@gmail.com>
2020-10-07 11:29:06 +0700

    Init order

--- backend/modules/iway/src/widgets/views/navbarWidgets.php
+++ backend/modules/iway/src/widgets/views/navbarWidgets.php
@@ -47,6 +47,12 @@ $routeInfos = [
         'label' => Yii::t('backend', 'View doctor'),
         'icon' => '<i class="glyphicon glyphicon-cog"></i>',
     ],
+    [
+        'module' => 'iway',
+        'controllerId' => 'order',
+        'label' => Yii::t('backend', 'Đơn hàng'),
+        'icon' => '<i class="glyphicon glyphicon-cog"></i>',
+    ],
 ];
 ?>
 
