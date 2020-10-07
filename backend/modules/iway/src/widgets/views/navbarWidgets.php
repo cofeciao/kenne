1aed136617f2374afd33787723ee4547abe24fa7
huynhlehoangduc <huynhlehoangduc@gmail.com>
2020-10-07 09:54:51 +0700

    Delete product

--- backend/modules/iway/src/widgets/views/navbarWidgets.php
+++ backend/modules/iway/src/widgets/views/navbarWidgets.php
@@ -47,12 +47,6 @@ $routeInfos = [
         'label' => Yii::t('backend', 'View doctor'),
         'icon' => '<i class="glyphicon glyphicon-cog"></i>',
     ],
-    [
-        'module' => 'iway',
-        'controllerId' => 'product',
-        'label' => Yii::t('backend', 'Sản phẩm'),
-        'icon' => '<i class="glyphicon glyphicon-cog"></i>',
-    ],
 ];
 ?>
 
